166a4f06a1ef28125cb21cc3cdbbcdebe1f710cd
FELIPE ESCOBAR AVILA <f.escobar@sis.virtual.uniandes.edu.co>
2018-02-11 12:50:29 -0500

    se hizo pago y factura

---
+++ viejitos-web/src/main/java/co/edu/uniandes/csw/viejitos/dtos/PagoDTO.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.viejitos.dtos;
+
+import java.util.Date;
+
+/**
+ *
+ * @author f.escobar
+ */
+public class PagoDTO {
+    
+    private String medio;
+    
+    private boolean pagado;
+    
+    private Date fechaLimite;
+    
+    private double valor;
+}
