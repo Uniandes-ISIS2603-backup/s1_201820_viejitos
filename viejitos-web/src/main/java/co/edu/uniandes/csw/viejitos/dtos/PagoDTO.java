6ec9e35ba6047cf7563547bb7b89d126df658a75
Felipe Escobar <f.escobar@sis.virtual.uniandes.edu.co>
2018-02-12 03:21:00 -0500

    getter y setters

--- viejitos-web/src/main/java/co/edu/uniandes/csw/viejitos/dtos/PagoDTO.java
+++ viejitos-web/src/main/java/co/edu/uniandes/csw/viejitos/dtos/PagoDTO.java
@@ -12,7 +12,7 @@ import java.util.Date;
  * @author f.escobar
  */
 public class PagoDTO {
-    
+
     private String medio;
     
     private boolean pagado;
@@ -20,6 +20,64 @@ public class PagoDTO {
     private Date fechaLimite;
     
     private double valor;
+    
+    /**
+     * @return the medio
+     */
+    public String getMedio() {
+        return medio;
+    }
+
+    /**
+     * @param medio the medio to set
+     */
+    public void setMedio(String medio) {
+        this.medio = medio;
+    }
+
+    /**
+     * @return the pagado
+     */
+    public boolean isPagado() {
+        return pagado;
+    }
+
+    /**
+     * @param pagado the pagado to set
+     */
+    public void setPagado(boolean pagado) {
+        this.pagado = pagado;
+    }
+
+    /**
+     * @return the fechaLimite
+     */
+    public Date getFechaLimite() {
+        return fechaLimite;
+    }
+
+    /**
+     * @param fechaLimite the fechaLimite to set
+     */
+    public void setFechaLimite(Date fechaLimite) {
+        this.fechaLimite = fechaLimite;
+    }
+
+    /**
+     * @return the valor
+     */
+    public double getValor() {
+        return valor;
+    }
+
+    /**
+     * @param valor the valor to set
+     */
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+    
+    
     
     public PagoDTO()
     {
