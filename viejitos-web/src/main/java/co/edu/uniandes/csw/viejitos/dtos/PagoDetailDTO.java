329a28452ee053d2c5fddcea5c084a32aa63de4d
FELIPE ESCOBAR AVILA <f.escobar@sis.virtual.uniandes.edu.co>
2018-02-11 19:07:27 -0500

    facturaDetail y PagoDetail

---
+++ viejitos-web/src/main/java/co/edu/uniandes/csw/viejitos/dtos/PagoDetailDTO.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.viejitos.dtos;
+
+/**
+ *
+ * @author f.escobar
+ */
+public class PagoDetailDTO extends PagoDTO {
+    
+    private ServicioDTO servicio;
+}
