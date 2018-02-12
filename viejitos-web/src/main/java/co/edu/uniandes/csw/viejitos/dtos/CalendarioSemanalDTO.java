7345b2698ffcf7017987d186f34c36c4420ce0fc
lf.naranjo11 <lf.naranjo11@ISIS2603S1-0081.sis.virtual.uniandes.edu.co>
2018-02-12 08:14:09 -0500

    none

---
+++ viejitos-web/src/main/java/co/edu/uniandes/csw/viejitos/dtos/CalendarioSemanalDTO.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.viejitos.dtos;
+
+/**
+ *
+ * @author lf.naranjo11
+ */
+public class CalendarioSemanalDTO {
+    
+    private long ultimaModficacion;
+    
+    
+    
+    private long getUltimaModificacion()
+            {
+                return ultimaModificacion;
+            }
+    private void setUltimaModificacion(long pUltimaModificacion;)
+            static {
+                ultimaModificacion=pUltimaModificacion;
+            }
+    
+    
+    public CalendarioSemanalDTO()
+    { 
+    }
+     
+     
+    
+}
