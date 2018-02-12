283cac6e6743a82fb69b880154774e2cb34832da
Laura Pardo <l.pardo@uniandes.edu.co>
2018-02-12 08:54:07 -0500

    MedicoDTO en teoria

---
+++ viejitos-web/src/main/java/co/edu/uniandes/csw/viejitos/dtos/MedicoDTO.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.viejitos.dtos;
+
+/**
+ *
+ * @author l.pardo
+ */
+public class MedicoDTO {
+
+    //atributos
+    private String nombre;
+    private Integer tipo;
+    private String id;
+    private String contrasena;
+   
+    public MedicoDTO()
+    {
+        
+    }
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the tipo
+     */
+    public Integer getTipo() {
+        return tipo;
+    }
+
+    /**
+     * @param tipo the tipo to set
+     */
+    public void setTipo(Integer tipo) {
+        this.tipo = tipo;
+    }
+
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the contrasena
+     */
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    /**
+     * @param contrasena the contrasena to set
+     */
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    
+}
