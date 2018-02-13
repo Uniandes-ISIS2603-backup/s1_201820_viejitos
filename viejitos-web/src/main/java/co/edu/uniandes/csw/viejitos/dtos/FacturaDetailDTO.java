c69074f438b3d06df15ef5f3d8efe2e711c2ac07
Felipe Escobar <f.escobar@sis.virtual.uniandes.edu.co>
2018-02-13 09:04:59 -0500

    json en detail

--- viejitos-web/src/main/java/co/edu/uniandes/csw/viejitos/dtos/FacturaDetailDTO.java
+++ viejitos-web/src/main/java/co/edu/uniandes/csw/viejitos/dtos/FacturaDetailDTO.java
@@ -6,7 +6,45 @@
 package co.edu.uniandes.csw.viejitos.dtos;
 
 /**
+ * FacturaDetailDTO Objeto de transferencia de datos de Facturas. Los DTO contienen las
+ * representaciones de los JSON que se transfieren entre el cliente y el
+ * servidor.
+ * 
+ * Al serializarse como JSON esta clase implementa el siguiente modelo: <br>
+ * <pre>
+ *   {
+ *      "id": Long,
+ *      "costoTotal": Double,
+ *      "fechaExpedicion": Date,
+ *      "descripcion": String,
+ *      "nombreCliente": String,
+ *      "ccCliente": Integer,
+ *      "servicioPrestado": String,
+ *      "nombreEmpresa": String,
+ *      "servicio": ServicioDTO
+ *   }
+ * </pre>
+ * Por ejemplo una ciudad se representa asi:<br>
+ * 
+ * <pre>
+ * 
+ *   {
+ *      "id": 123,
+ *      "costoTotal": 120100,
+ *      "fechaExpedicion": 12/04/18,
+ *      "descripcion": "Acompanar al banco",
+ *      "nombreCliente": "Felipe Escobar",
+ *      "ccCliente": 1234567,
+ *      "servicioPrestado": "Acompanamiento",
+ *      "nombreEmpresa": "Hospital"
+ *      "servicio": {"tipo": 1,
+ *      "fecha: "12/02/2018",
+ *      "hora": "8:37",
+ *      "descripción": "Acompañamiento a cita médica",
+ *      "finalizado": true}
+ *   }
  *
+ * </pre>
  * @author f.escobar
  */
 public class FacturaDetailDTO extends FacturaDTO {
--- viejitos-web/src/main/java/co/edu/uniandes/csw/viejitos/dtos/PagoDetailDTO.java
+++ viejitos-web/src/main/java/co/edu/uniandes/csw/viejitos/dtos/PagoDetailDTO.java
@@ -8,6 +8,38 @@ package co.edu.uniandes.csw.viejitos.dtos;
 /**
  *
  * @author f.escobar
+ * PagoDetailDTO Objeto de transferencia de datos de Pagos. Los DTO contienen las
+ represnetaciones de los JSON que se transfieren entre el cliente y el
+ servidor.
+ 
+ Al serializarse como JSON esta clase implementa el siguiente modelo: <br>
+ <pre>
+   {
+      "id": Long,
+      "medio": String,
+      "pagado": Boolean,
+      "fechaLimite": Date,
+      "valor": Double,
+      "servicio": ServicioDTO
+   }
+ </pre>
+ Por ejemplo una ciudad se representa asi:<br>
+ 
+ <pre>
+ 
+   {
+      "id": 123456,
+      "medio": "Efectivo",
+      "pagado": TRUE,
+      "fechaLimite": 12/04/18,
+      "valor": 150000
+      "servicio": {"tipo": 1,
+ *      "fecha: "12/02/2018",
+ *      "hora": "8:37",
+ *      "descripción": "Acompañamiento a cita médica",
+ *      "finalizado": true}
+   }
+   </pre>
  */
 public class PagoDetailDTO extends PagoDTO {
 
