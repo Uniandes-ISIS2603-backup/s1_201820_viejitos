8d940ba7093c1fac02e277db8b4ac9f68ebd0120
camila <c.gomezs@sis.virtual.uniandes.edu.co>
2018-02-12 21:25:45 -0500

    Se actualizaron los recursos de servicio y queja

--- viejitos-web/src/main/java/co/edu/uniandes/csw/viejitos/resources/ServicioResource.java
+++ viejitos-web/src/main/java/co/edu/uniandes/csw/viejitos/resources/ServicioResource.java
@@ -20,14 +20,26 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 /**
- * <pre>Clase que implementa el recurso "Servicio".
- * URL: /api/Servicio
+ * <pre>Clase que implementa el recurso "servicios".
+ * URL: /api/servicios
+ * </pre>
+ * <i>Note que la aplicación (definida en {@link RestConfig}) define la ruta "/api" y
+ * este recurso tiene la ruta "servicios".</i>
+ *
+ * <h2>Anotaciones </h2>
+ * <pre>
+ * Path: indica la dirección después de "api" para acceder al recurso
+ * Produces/Consumes: indica que los servicios definidos en este recurso reciben y devuelven objetos en formato JSON
+ * RequestScoped: Inicia una transacción desde el llamado de cada método (servicio). 
+ * </pre>
  * @author c.gomezs
+ * @version 1.0
  */
 @Path( "Servicios" )
 @Produces( "application/json" )
 @Consumes( "application/json" )
 @RequestScoped
+
 public class ServicioResource
 {
     /**
