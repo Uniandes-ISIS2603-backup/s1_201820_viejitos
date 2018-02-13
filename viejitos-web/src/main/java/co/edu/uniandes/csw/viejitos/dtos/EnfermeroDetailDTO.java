f7a27712accb7c60ebb5b65ec979e52c9bf851ac
JUAN SEBASTIAN ESPITIA ACERO <js.espitia@sis.virtual.uniandes.edu.co>
2018-02-13 09:03:45 -0500

    Documentacion de detailDTO para calificacion y enfermero. Ejemplos de JSON

--- viejitos-web/src/main/java/co/edu/uniandes/csw/viejitos/dtos/EnfermeroDetailDTO.java
+++ viejitos-web/src/main/java/co/edu/uniandes/csw/viejitos/dtos/EnfermeroDetailDTO.java
@@ -8,7 +8,35 @@ package co.edu.uniandes.csw.viejitos.dtos;
 import java.util.List;
 
 /**
- *
+ * Objeto de transferencia de datos con relaciones para un enfermero.
+ * Al serializarse como DTO se genera un JSON de la siguiente manera
+ * <pre>
+ *  {
+ *      "id":number,
+ *      "nombre":string,
+ *      "cv":string,
+ *      "login":string,
+ *      "contrasenia":string,
+ *      "tipo":string,
+ *      "calificaciones":[calificaciones],
+ *      "servicios":[servicios]
+ *  }
+ * </pre>
+ * 
+ * Por ejemplo, un enfermero se representa asi:
+ * <pre>
+ *  {
+ *      "id":2678389,
+ *      "nombre":"Juan Espitia",
+ *      "cv":"./documents/juanHDV.pdf",
+ *      "login":,"js.espitia"
+ *      "contrasenia":"a89ess0909",
+ *      "tipo":"Enfermero",
+ *      "calificaciones":[{"id":209873, "puntaje":4.5,"comentario":"El enfermero fue grosero conmigo, se rehuso a prestarme un servicio adecuado","tipoCalificador":"cliente",
+"loginCalificador":"af.ramirez","tipoCalificado":"enfermero","loginCalificado":"ma.marulanda"}]
+ *      "servicios":[{ "tipo": 2,"fecha": 03/05/2017,"hora": "9:30","descripción": "Acompañamiento a cita","finalizado": true}]
+ *  }
+ * </pre>
  * @author js.espitia
  */
 public class EnfermeroDetailDTO extends EnfermeroDTO{
