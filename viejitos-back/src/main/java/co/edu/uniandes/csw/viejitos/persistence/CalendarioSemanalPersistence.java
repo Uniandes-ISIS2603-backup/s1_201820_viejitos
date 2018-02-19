74f8f097dde968df50a105891047fd7793bda1cb
lf.naranjo11 <lf.naranjo11@ISIS2603S1-0081.sis.virtual.uniandes.edu.co>
2018-02-18 23:26:53 -0500

    none

--- viejitos-back/src/main/java/co/edu/uniandes/csw/viejitos/persistence/CalendarioSemanalPersistence.java
+++ viejitos-back/src/main/java/co/edu/uniandes/csw/viejitos/persistence/CalendarioSemanalPersistence.java
@@ -5,10 +5,47 @@
  */
 package co.edu.uniandes.csw.viejitos.persistence;
 
+import co.edu.uniandes.csw.viejitos.entities.CalendarioSemanalEntity;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 /**
  *
  * @author lf.naranjo11
  */
+
+@Stateless
 public class CalendarioSemanalPersistence {
+    
+    
+    private static final Logger LOGGER= Logger.getLogger(CalendarioSemanalPersistence.class.getName());
+      @PersistenceContext(unitName="CompanyPU")
+    protected EntityManager em;
+      
+      public CalendarioSemanalEntity create(CalendarioSemanalEntity entity)
+      {  
+         LOGGER.info("creando calendario semanal");
+         em.persist(entity);
+        LOGGER.info("calendario semanal creado");
+        return entity;
+       }
+      
+       public CalendarioSemanalEntity update(CalendarioSemanalEntity entity) {
+        LOGGER.log(Level.INFO, "Actualizando calendario Semanal con id={0}", entity.getId());
+        return em.merge(entity);
+    }
+
+    public void delete(Long id) {
+        LOGGER.log(Level.INFO, "Borrando calendario Semanal con id={0}", id);
+        CalendarioSemanalEntity entity = em.find(CalendarioSemanalEntity.class, id);
+        em.remove(entity);
+    }
+      
+       public CalendarioSemanalEntity find()
+      {   return null;
+       }
     
 }
--- viejitos-back/src/main/java/co/edu/uniandes/csw/viejitos/persistence/FranjaHorariaPersistence.java
+++ viejitos-back/src/main/java/co/edu/uniandes/csw/viejitos/persistence/FranjaHorariaPersistence.java
@@ -5,10 +5,72 @@
  */
 package co.edu.uniandes.csw.viejitos.persistence;
 
+import co.edu.uniandes.csw.viejitos.entities.FranjaHorariaEntity;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+
 /**
  *
  * @author lf.naranjo11
  */
+@Stateless
 public class FranjaHorariaPersistence {
+    
+      @PersistenceContext(unitName="CompanyPU")
+    protected EntityManager em;
+      
+      
+      private static final Logger LOGGER = Logger.getLogger(FranjaHorariaPersistence.class.getName());
+      
+      public FranjaHorariaEntity create(FranjaHorariaEntity entity)
+     {
+          LOGGER.info("creando franja horaria");
+          em.persist(entity);
+           LOGGER.info("Franja horaria creado");
+          return entity;
+      }
+      
+      
+      public FranjaHorariaEntity find(Long id)
+      {
+       LOGGER.log(Level.INFO, "Consultando employee con id={0}", id);
+        return em.find(FranjaHorariaEntity.class, id);   
+      }
+      
+      public List<FranjaHorariaEntity> findAll()
+      {
+          LOGGER.info("buscando todas las franjas horarias");
+         Query q=em.createQuery("select u from FranjaHorariaEntity u");
+         return q.getResultList();
+      }
+      
+      public List<FranjaHorariaEntity> findByDay(String dia)
+      {
+          LOGGER.log(Level.INFO,"buscando franjas del dia=",dia);
+          
+          TypedQuery<FranjaHorariaEntity> q = em.createQuery("select u from FranjaHorariaEntity where u.getDiaSemana = :dia",FranjaHorariaEntity.class);
+     // q = q.setParameter("diaSemana", dia);     
+     return q.getResultList();
+          
+      }
+    public FranjaHorariaEntity update(FranjaHorariaEntity entity)
+    {
+    LOGGER.log(Level.INFO,"actualizando franja con id={0}",entity.getId());
+    return em.merge(entity);
+  
+    }
+    
+    public void delete(Long id)
+    {
+        LOGGER.log(Level.INFO,"Borrando franja con id={0}",id);
+        FranjaHorariaEntity entity=em.find(FranjaHorariaEntity.class, id);
+        em.remove(entity);
+    }
     
 }
