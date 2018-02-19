2ef4a511a2bf020f3f1440050e3c384ff4885ffa
lf.naranjo11 <lf.naranjo11@ISIS2603S1-0081.sis.virtual.uniandes.edu.co>
2018-02-18 20:37:58 -0500

    none

--- viejitos-back/src/main/java/co/edu/uniandes/csw/viejitos/entities/FranjaHorariaEntity.java
+++ viejitos-back/src/main/java/co/edu/uniandes/csw/viejitos/entities/FranjaHorariaEntity.java
@@ -6,11 +6,89 @@
 package co.edu.uniandes.csw.viejitos.entities;
 
 import java.io.Serializable;
+import javax.persistence.Entity;
 
 /**
  *
  * @author lf.naranjo11
  */
+@Entity
 public class FranjaHorariaEntity extends BaseEntity implements Serializable {
+     private Integer horaInicio;
+    private Integer horaFin;
+    private Boolean ocupado;
+    private String diaSemana;
+    
+    private Long id;
+
+    /**
+     * @return the horaInicio
+     */
+    public Integer getHoraInicio() {
+        return horaInicio;
+    }
+
+    /**
+     * @param horaInicio the horaInicio to set
+     */
+    public void setHoraInicio(Integer horaInicio) {
+        this.horaInicio = horaInicio;
+    }
+
+    /**
+     * @return the horaFin
+     */
+    public Integer getHoraFin() {
+        return horaFin;
+    }
+
+    /**
+     * @param horaFin the horaFin to set
+     */
+    public void setHoraFin(Integer horaFin) {
+        this.horaFin = horaFin;
+    }
+
+    /**
+     * @return the ocupado
+     */
+    public Boolean getOcupado() {
+        return ocupado;
+    }
+
+    /**
+     * @param ocupado the ocupado to set
+     */
+    public void setOcupado(Boolean ocupado) {
+        this.ocupado = ocupado;
+    }
+
+    /**
+     * @return the diaSemana
+     */
+    public String getDiaSemana() {
+        return diaSemana;
+    }
+
+    /**
+     * @param diaSemana the diaSemana to set
+     */
+    public void setDiaSemana(String diaSemana) {
+        this.diaSemana = diaSemana;
+    }
+
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
     
 }
