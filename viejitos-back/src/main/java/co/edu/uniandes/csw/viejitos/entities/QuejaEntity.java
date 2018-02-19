2e352bd6f04747540a98d5f89fc4f2b40ce78989
camila <c.gomezs@sis.virtual.uniandes.edu.co>
2018-02-18 19:43:24 -0500

    Se realizaron las entidades de servicio y queja

---
+++ viejitos-back/src/main/java/co/edu/uniandes/csw/viejitos/entities/QuejaEntity.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.viejitos.entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author c.gomezs
+ */
+@Entity
+public class QuejaEntity extends BaseEntity implements Serializable{
+    
+    private String reclamo;
+    
+    private Boolean resuelto;
+    
+    private Long id;
+    
+    /**
+     * @return the reclamo
+     */
+    public String getReclamo() {
+        return reclamo;
+    }
+
+    /**
+     * @param reclamo the reclamo to set
+     */
+    public void setReclamo(String reclamo) {
+        this.reclamo = reclamo;
+    }
+
+    /**
+     * @return the resuelto
+     */
+    public Boolean getResuelto() {
+        return resuelto;
+    }
+
+    /**
+     * @param resuelto the resuelto to set
+     */
+    public void setResuelto(Boolean resuelto) {
+        this.resuelto = resuelto;
+    }
+
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    
+    
+}
---
+++ viejitos-back/src/main/java/co/edu/uniandes/csw/viejitos/entities/ServicioEntity.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.viejitos.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author c.gomezs
+ */
+
+@Entity
+public class ServicioEntity extends BaseEntity implements Serializable{
+
+    private Integer tipo;
+    
+    private Date fecha;
+    
+    private String hora;
+    
+    private String descripcion;
+    
+    private Boolean finalizado;
+    
+    private Long id;
+    
+    /**
+     * @return the tipo
+     */
+    public Integer getTipo() {
+        return tipo;
+    }
+
+    /**
+     * @param tipo the tipo to set
+     */
+    public void setTipo(Integer tipo) {
+        this.tipo = tipo;
+    }
+
+    /**
+     * @return the fecha
+     */
+    public Date getFecha() {
+        return fecha;
+    }
+
+    /**
+     * @param fecha the fecha to set
+     */
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    /**
+     * @return the hora
+     */
+    public String getHora() {
+        return hora;
+    }
+
+    /**
+     * @param hora the hora to set
+     */
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    /**
+     * @return the descripcion
+     */
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    /**
+     * @param descripcion the descripcion to set
+     */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    /**
+     * @return the finalizado
+     */
+    public Boolean getFinalizado() {
+        return finalizado;
+    }
+
+    /**
+     * @param finalizado the finalizado to set
+     */
+    public void setFinalizado(Boolean finalizado) {
+        this.finalizado = finalizado;
+    }
+
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+}
