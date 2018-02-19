a3e0e5e069997034736d6fd802f3a0cc8d03e30b
lf.naranjo11 <lf.naranjo11@ISIS2603S1-0081.sis.virtual.uniandes.edu.co>
2018-02-18 20:39:01 -0500

    none

--- viejitos-back/src/main/java/co/edu/uniandes/csw/viejitos/entities/CalendarioSemanalEntity.java
+++ viejitos-back/src/main/java/co/edu/uniandes/csw/viejitos/entities/CalendarioSemanalEntity.java
@@ -6,12 +6,46 @@
 package co.edu.uniandes.csw.viejitos.entities;
 
 import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
 
 /**
  *
  * @author lf.naranjo11
  */
+@Entity
 public class CalendarioSemanalEntity extends BaseEntity implements Serializable
 {
+    private Long id;
+    private Date ultimaModficacion;
+
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the ultimaModficacion
+     */
+    public Date getUltimaModficacion() {
+        return ultimaModficacion;
+    }
+
+    /**
+     * @param ultimaModficacion the ultimaModficacion to set
+     */
+    public void setUltimaModficacion(Date ultimaModficacion) {
+        this.ultimaModficacion = ultimaModficacion;
+    }
+    
     
 }
