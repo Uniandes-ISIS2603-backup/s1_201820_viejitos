89d308f36b092b6a5ec8383543437e982ec6a435
camila <c.gomezs@sis.virtual.uniandes.edu.co>
2018-02-25 23:16:35 -0500

    Se realizaron los test de persistencia de queja y servicio

--- viejitos-back/src/test/java/co/edu/uniandes/csw/viejitos/test/persistence/QuejaPersistenceTest.java
+++ viejitos-back/src/test/java/co/edu/uniandes/csw/viejitos/test/persistence/QuejaPersistenceTest.java
@@ -8,11 +8,20 @@ package co.edu.uniandes.csw.viejitos.test.persistence;
 
 import co.edu.uniandes.csw.viejitos.entities.QuejaEntity;
 import co.edu.uniandes.csw.viejitos.persistence.QuejaPersistence;
+import java.util.ArrayList;
+import java.util.List;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import uk.co.jemos.podam.api.PodamFactory;
 import uk.co.jemos.podam.api.PodamFactoryImpl;
 
@@ -20,7 +29,24 @@ import uk.co.jemos.podam.api.PodamFactoryImpl;
  *
  * @author c.gomezs
  */
+
+@RunWith(Arquillian.class)
 public class QuejaPersistenceTest {
+    
+     /**
+     * @return Devuelve el jar que Arquillian va a desplegar en el Glassfish
+     * embebido. El jar contiene las clases de Queja, el descriptor de la
+     * base de datos y el archivo beans.xml para resolver la inyección de
+     * dependencias.
+     */
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addPackage(QuejaEntity.class.getPackage())
+                .addPackage(QuejaPersistence.class.getPackage())
+                .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
+                .addAsManifestResource("META-INF/beans.xml", "beans.xml");
+    }
     
     /**
      * Contexto de Persistencia que se va a utilizar para acceder a la Base de
@@ -36,6 +62,65 @@ public class QuejaPersistenceTest {
     @Inject
     private QuejaPersistence quejaPersistence;
     
+     /**
+     * Limpia las tablas que están implicadas en la prueba.
+     */
+    private void clearData() {
+        em.createQuery("delete from QuejaEntity").executeUpdate();
+    }
+    
+     /**
+     * Lista que tiene los datos de prueba.
+     */
+    private List<QuejaEntity> data = new ArrayList<QuejaEntity>();
+    
+    /**
+     * Variable para marcar las transacciones del em anterior cuando se
+     * crean/borran datos para las pruebas.
+     */
+    @Inject
+    UserTransaction utx;
+    
+    /**
+     * Inserta los datos iniciales para el correcto funcionamiento de las
+     * pruebas.
+     */
+    private void insertData() {
+        PodamFactory factory = new PodamFactoryImpl();
+        for (int i = 0; i < 3; i++) {
+            
+            QuejaEntity entity = factory.manufacturePojo(QuejaEntity.class);
+
+            em.persist(entity);
+            
+            data.add(entity);
+        }
+    }
+    
+    /**
+     * Configuración inicial de la prueba.
+     */
+    @Before
+    public void configTest() {
+        try {
+            utx.begin();
+            em.joinTransaction();
+            clearData();
+            insertData();
+            utx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                utx.rollback();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+    
+    /**
+     * Prueba para crear una queja.
+     */
     @Test
     public void createQuejaEntityTest() {
     PodamFactory factory = new PodamFactoryImpl();
@@ -47,5 +132,63 @@ public class QuejaPersistenceTest {
     Assert.assertNotNull(entity);
     Assert.assertEquals(newEntity.getName(), entity.getName());
 }
+    
+    /**
+     * Prueba para consultar la lista de quejas. 
+     */
+    @Test
+    public void getQuejasTest() {
+        List<QuejaEntity> list = quejaPersistence.findAll();
+        Assert.assertEquals(data.size(), list.size());
+        for (QuejaEntity ent : list) {
+            boolean found = false;
+            for (QuejaEntity entity : data) {
+                if (ent.getId().equals(entity.getId())) {
+                    found = true;
+                }
+            }
+            Assert.assertTrue(found);
+        }
+    }
+    
+     /**
+     * Prueba para consultar una queja.
+     */
+    @Test
+    public void getQuejaTest() {
+        QuejaEntity entity = data.get(0);
+        QuejaEntity newEntity = quejaPersistence.find(entity.getId());
+        Assert.assertNotNull(newEntity);
+        Assert.assertEquals(entity.getName(), newEntity.getName());
+    }
+
+    /**
+     * Prueba para eliminar una queja.
+     */
+    @Test
+    public void deleteQuejaTest() {
+        QuejaEntity entity = data.get(0);
+        quejaPersistence.delete(entity.getId());
+        QuejaEntity deleted = em.find(QuejaEntity.class, entity.getId());
+        Assert.assertNull(deleted);
+    }
+
+    /**
+     * Prueba para actualizar una queja.
+     */
+    @Test
+    public void updateQuejaTest() {
+        QuejaEntity entity = data.get(0);
+        PodamFactory factory = new PodamFactoryImpl();
+        QuejaEntity newEntity = factory.manufacturePojo(QuejaEntity.class);
+
+        newEntity.setId(entity.getId());
+
+        quejaPersistence.update(newEntity);
+
+        QuejaEntity resp = em.find(QuejaEntity.class, entity.getId());
+
+        Assert.assertEquals(newEntity.getName(), resp.getName());
+    }
     
 }
--- viejitos-back/src/test/java/co/edu/uniandes/csw/viejitos/test/persistence/ServicioPersistenceTest.java
+++ viejitos-back/src/test/java/co/edu/uniandes/csw/viejitos/test/persistence/ServicioPersistenceTest.java
@@ -6,11 +6,20 @@
 package co.edu.uniandes.csw.viejitos.test.persistence;
 import co.edu.uniandes.csw.viejitos.entities.ServicioEntity;
 import co.edu.uniandes.csw.viejitos.persistence.ServicioPersistence;
+import java.util.ArrayList;
+import java.util.List;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import uk.co.jemos.podam.api.PodamFactory;
 import uk.co.jemos.podam.api.PodamFactoryImpl;
 
@@ -18,7 +27,24 @@ import uk.co.jemos.podam.api.PodamFactoryImpl;
  *
  * @author c.gomezs
  */
+
+@RunWith(Arquillian.class)
 public class ServicioPersistenceTest {
+    
+    /**
+     * @return Devuelve el jar que Arquillian va a desplegar en el Glassfish
+     * embebido. El jar contiene las clases de Servicio, el descriptor de la
+     * base de datos y el archivo beans.xml para resolver la inyección de
+     * dependencias.
+     */
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addPackage(ServicioEntity.class.getPackage())
+                .addPackage(ServicioPersistence.class.getPackage())
+                .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
+                .addAsManifestResource("META-INF/beans.xml", "beans.xml");
+    }
     
     /**
      * Contexto de Persistencia que se va a utilizar para acceder a la Base de
@@ -34,6 +60,65 @@ public class ServicioPersistenceTest {
     @Inject
     private ServicioPersistence servicioPersistence;
     
+    /**
+     * Limpia las tablas que están implicadas en la prueba.
+     */
+    private void clearData() {
+        em.createQuery("delete from ServicioEntity").executeUpdate();
+    }
+    
+     /**
+     * Lista que tiene los datos de prueba.
+     */
+    private List<ServicioEntity> data = new ArrayList<ServicioEntity>();
+    
+    /**
+     * Variable para marcar las transacciones del em anterior cuando se
+     * crean/borran datos para las pruebas.
+     */
+    @Inject
+    UserTransaction utx;
+    
+    /**
+     * Inserta los datos iniciales para el correcto funcionamiento de las
+     * pruebas.
+     */
+    private void insertData() {
+        PodamFactory factory = new PodamFactoryImpl();
+        for (int i = 0; i < 3; i++) {
+            
+            ServicioEntity entity = factory.manufacturePojo(ServicioEntity.class);
+
+            em.persist(entity);
+            
+            data.add(entity);
+        }
+    }
+    
+    /**
+     * Configuración inicial de la prueba.
+     */
+    @Before
+    public void configTest() {
+        try {
+            utx.begin();
+            em.joinTransaction();
+            clearData();
+            insertData();
+            utx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                utx.rollback();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+    
+    /**
+     * Prueba para crear un servicio.
+     */
     @Test
     public void createServicioEntityTest() {
     PodamFactory factory = new PodamFactoryImpl();
@@ -44,5 +129,63 @@ public class ServicioPersistenceTest {
     ServicioEntity entity = em.find(ServicioEntity.class, result.getId());
     Assert.assertNotNull(entity);
     Assert.assertEquals(newEntity.getName(), entity.getName());
+    }
+    
+    /**
+     * Prueba para consultar la lista de servicios. 
+     */
+    @Test
+    public void getServiciosTest() {
+        List<ServicioEntity> list = servicioPersistence.findAll();
+        Assert.assertEquals(data.size(), list.size());
+        for (ServicioEntity ent : list) {
+            boolean found = false;
+            for (ServicioEntity entity : data) {
+                if (ent.getId().equals(entity.getId())) {
+                    found = true;
+                }
+            }
+            Assert.assertTrue(found);
+        }
+    }
+    
+     /**
+     * Prueba para consultar un servicio.
+     */
+    @Test
+    public void getServicioTest() {
+        ServicioEntity entity = data.get(0);
+        ServicioEntity newEntity = servicioPersistence.find(entity.getId());
+        Assert.assertNotNull(newEntity);
+        Assert.assertEquals(entity.getName(), newEntity.getName());
+    }
+
+    /**
+     * Prueba para eliminar un servicio.
+     */
+    @Test
+    public void deleteServicioTest() {
+        ServicioEntity entity = data.get(0);
+        servicioPersistence.delete(entity.getId());
+        ServicioEntity deleted = em.find(ServicioEntity.class, entity.getId());
+        Assert.assertNull(deleted);
+    }
+
+    /**
+     * Prueba para actualizar un servicio.
+     */
+    @Test
+    public void updateServicioTest() {
+        ServicioEntity entity = data.get(0);
+        PodamFactory factory = new PodamFactoryImpl();
+        ServicioEntity newEntity = factory.manufacturePojo(ServicioEntity.class);
+
+        newEntity.setId(entity.getId());
+
+        servicioPersistence.update(newEntity);
+
+        ServicioEntity resp = em.find(ServicioEntity.class, entity.getId());
+
+        Assert.assertEquals(newEntity.getName(), resp.getName());
     }
 }
