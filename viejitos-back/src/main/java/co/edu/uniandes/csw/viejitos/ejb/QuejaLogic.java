2f48513932b2e4c113b5365c98f104077cfe1b81
camila <c.gomezs@sis.virtual.uniandes.edu.co>
2018-02-25 23:44:08 -0500

    Se realizo la clase de logica de servicio

--- viejitos-back/src/main/java/co/edu/uniandes/csw/viejitos/ejb/QuejaLogic.java
+++ viejitos-back/src/main/java/co/edu/uniandes/csw/viejitos/ejb/QuejaLogic.java
@@ -4,7 +4,6 @@
  * and open the template in the editor.
  */
 package co.edu.uniandes.csw.viejitos.ejb;
-
 import co.edu.uniandes.csw.viejitos.entities.QuejaEntity;
 import co.edu.uniandes.csw.viejitos.exceptions.BusinessLogicException;
 import co.edu.uniandes.csw.viejitos.persistence.QuejaPersistence;
