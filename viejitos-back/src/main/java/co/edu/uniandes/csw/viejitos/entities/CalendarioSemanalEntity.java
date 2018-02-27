a60aa5cf88ce5e5b788e5f25c3921316000b7eb4
lf.naranjo11 <lf.naranjo11@ISIS2603S1-0081.sis.virtual.uniandes.edu.co>
2018-02-27 08:59:37 -0500

    creaccion relaciones OneTomany,OneToOne,etc

--- viejitos-back/src/main/java/co/edu/uniandes/csw/viejitos/entities/CalendarioSemanalEntity.java
+++ viejitos-back/src/main/java/co/edu/uniandes/csw/viejitos/entities/CalendarioSemanalEntity.java
@@ -6,13 +6,19 @@
 package co.edu.uniandes.csw.viejitos.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import uk.co.jemos.podam.common.PodamExclude;
 
 /**
  *
@@ -24,7 +30,15 @@ public class CalendarioSemanalEntity extends BaseEntity implements Serializable
    @Temporal(TemporalType.DATE)
     private Date ultimaModficacion;
 
+   @PodamExclude
+   @OneToMany(cascade=CascadeType.ALL,orphanRemoval=true,mappedBy= "calendario", targetEntity= FranjaHorariaEntity.class )
+  private List<FranjaHorariaEntity> franjas=new ArrayList<FranjaHorariaEntity>();
    
+   @OneToOne
+   private EnfermeroEntity enfermero;
+   
+   @OneToOne(mappedBy="calendario",targetEntity=MedicoEntity.class)
+   private MedicoEntity medico;
 
     /**
      * @return the ultimaModficacion
--- viejitos-back/src/main/java/co/edu/uniandes/csw/viejitos/entities/FranjaHorariaEntity.java
+++ viejitos-back/src/main/java/co/edu/uniandes/csw/viejitos/entities/FranjaHorariaEntity.java
@@ -7,6 +7,7 @@ package co.edu.uniandes.csw.viejitos.entities;
 
 import java.io.Serializable;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 
 /**
  *
@@ -20,6 +21,8 @@ public class FranjaHorariaEntity extends BaseEntity implements Serializable {
     private String diaSemana;
     
     
+@ManyToOne
+private CalendarioSemanalEntity calendario;
 
     /**
      * @return the horaInicio
@@ -59,7 +62,7 @@ public class FranjaHorariaEntity extends BaseEntity implements Serializable {
     /**
      * @param ocupado the ocupado to set
      */
-    public void setOcupado(Boolean ocupado) {
+    public void isOcupado(Boolean ocupado) {
         this.ocupado = ocupado;
     }
 
