633a64fc8c6ff98d3e9263500a1530b944741cd8
jj.silva <jj.silva@ISIS2603S1-0076.sis.virtual.uniandes.edu.co>
2018-02-27 08:47:20 -0500

    Se agregaron los códigos de error en las clases ClienteResource e HistoriaClinicaResource. También se agregó la regla de negocio para HistoriaClinicaLogic.

--- viejitos-web/src/main/java/co/edu/uniandes/csw/viejitos/resources/HistoriaClinicaResource.java
+++ viejitos-web/src/main/java/co/edu/uniandes/csw/viejitos/resources/HistoriaClinicaResource.java
@@ -38,6 +38,9 @@ public class HistoriaClinicaResource
     /**
 	 * <h1>POST /api/historiasc : Crear una entidad de HistoriaClinica.</h1>
 	 * <pre>Cuerpo de petición: JSON {@link HistoriaClinicaDetailDTO}.
+         * * Codigos de respuesta:
+         * <code style="color: mediumseagreen; background-color: #eaffe0;">
+         * 200 OK Crea la HistoriaClinica.</code> 
 	 *</pre>
 	 * Crea una nueva entidad de HistoriaClinica con la informacion que se recibe en el cuerpo
 	 * de la petición.
@@ -53,6 +56,9 @@ public class HistoriaClinicaResource
         /**
 	 * <h1>GET /api/historiasc : Obtener todas las entidades de HistoriaClinica.</h1>
 	 * <pre>Busca y devuelve todas las entidades de HistoriaClinica que existen en la aplicacion.
+         * Codigos de respuesta:
+         * <code style="color: mediumseagreen; background-color: #eaffe0;">
+         * 200 OK Devuelve las historias clinicas..</code>
          * </pre>
 	 * @return JSONArray {@link HistoriaClinicaDetailDTO} - Las entidades de HistoriaClinica encontradas en la aplicación. Si no hay ninguna retorna una lista vacía.
 	 */
@@ -65,6 +71,12 @@ public class HistoriaClinicaResource
         /**
 	 * <h1>GET /api/historiasc/{id} : Obtener una entidad de HistoriaClinica por id.</h1>
 	 * <pre>Busca la entidad de HistoriaClinica con el id asociado recibido en la URL y la devuelve.
+         * Codigos de respuesta:
+         * <code style="color: mediumseagreen; background-color: #eaffe0;">
+         * 200 OK Devuelve la hisotria clinica.</code> 
+         * <code style="color: #c7254e; background-color: #f9f2f4;">
+         * 404 Not Found. No existe una Historia Clinica con el id dado.
+         * </code>
          * </pre>
 	 * @param id Identificador de la entidad de HistoriaClinica que se esta buscando. Este debe ser una cadena de dígitos.
 	 * @return JSON {@link HistoriaClinicaDetailDTO} - La entidad de HistoriaClinica buscada
@@ -79,6 +91,12 @@ public class HistoriaClinicaResource
         /**
 	 * <h1>PUT /api/historiasc/{id} : Actualizar una entidad de HistoriaClinica con el id dado.</h1>
 	 * <pre>Cuerpo de petición: JSON {@link HistoriaClinicaDetailDTO}.
+         * Codigos de respuesta:
+         * <code style="color: mediumseagreen; background-color: #eaffe0;">
+         * 200 OK Actualiza la Historia Clinica con el id dado con la información enviada como parámetro. Retorna un objeto identico.</code> 
+         * <code style="color: #c7254e; background-color: #f9f2f4;">
+         * 404 Not Found. No existe una Historia Clinica con el id dado.
+         * </code> 
 	 *</pre>
 	 * Actualiza la entidad de HistoriaClinica con el id recibido en la URL con la informacion que se recibe en el cuerpo de la petición.
 	 * @param id        Identificador de la entidad de HistoriaClinica que se desea actualizar. Este debe ser una cadena de dígitos.
@@ -96,6 +114,11 @@ public class HistoriaClinicaResource
 	 * <h1>DELETE /api/historiasc/{id} : Borrar una entidad de HistoriaClinica por id.</h1>
 	 * <p>
 	 * <pre>Borra la entidad de HistoriaClinica con el id asociado recibido en la URL.
+         * Códigos de respuesta:<br>
+         * <code style="color: mediumseagreen; background-color: #eaffe0;">
+         * 200 OK Elimina la Historia Clinica correspondiente al id dado.</code>
+         * <code style="color: #c7254e; background-color: #f9f2f4;">
+         * 404 Not Found. No existe una Historia Clinica con el id dado.</code>
 	 * </pre>
 	 *
 	 * @param id Identificador de la entidad de HistoriaClinica que se desea borrar. Este debe ser una cadena de dígitos.
