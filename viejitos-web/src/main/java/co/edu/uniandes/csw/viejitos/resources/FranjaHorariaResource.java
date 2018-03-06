005badc6dc3e959062064a8ebf06b1662971e93c
lf.naranjo11 <lf.naranjo11@ISIS2603S1-0081.sis.virtual.uniandes.edu.co>
2018-03-05 19:20:07 -0500

    doble metodo anotado

--- viejitos-web/src/main/java/co/edu/uniandes/csw/viejitos/resources/FranjaHorariaResource.java
+++ viejitos-web/src/main/java/co/edu/uniandes/csw/viejitos/resources/FranjaHorariaResource.java
@@ -133,23 +133,7 @@ public class FranjaHorariaResource {
 		
 	}
         
-         /**
-	 * <h1>GET /api/franjasHorarias/dia : Obtener todas las entidades de franja horaria de un dia.</h1>
-	 * <pre>Busca y devuelve todas las entidades de franja que se relacion a un dia dado.  
-     * 
-     * Codigos de respuesta:
-     * <code style="color: mediumseagreen; background-color: #eaffe0;">
-     * 200 OK Devuelve todas las ciudades de la aplicacion.</code> 
-     * </pre>
-         * 
-	 * @return JSONArray {@link FranjaHorariaDTO} - Las entidades de calendario semanal encontradas en la aplicación.
-	 */
-	@GET
-	public List<FranjaHorariaDTO> getFranjas( )
-	{
-		return new ArrayList<FranjaHorariaDTO>();
-	}
-        
+      
       /**
        * <h1> PUT api/franjashorarias/{id}: actualizar la franja con el id dado</h1>
      * <pre>Cuerpo de peticion: JSON {@link FranjaHorariaDTO}.
