e99295d1728b6a876c620425e7aad85c3d2ae2bc
lf.naranjo11 <lf.naranjo11@ISIS2603S1-0081.sis.virtual.uniandes.edu.co>
2018-04-23 05:03:49 -0500

    jsons incompletos, se implemento re3(hay varias convenciones en el manejo de los JSON poconaturales)

--- viejitos-web/src/main/webapp/src/modules/enfermeros/calendario.detail.ctrl.js
+++ viejitos-web/src/main/webapp/src/modules/enfermeros/calendario.detail.ctrl.js
@@ -1,3 +1,22 @@
-
+(function(ng){
+    var mod = ng.module('enfermerosModule');
+    mod.constant('enfermeroContext','api/enfermeros');
+    mod.controller('calendariosDetailCtrl', ['$scope', '$http', 'enfermeroContext','$state',
+        function($scope, $http, enfermeroContext, $state){
+             if($state.params.enfermeroId!==null &&$state.params.enfermeroId!==undefined &&$state.params.franjaId!==null &&$state.params.franjaId!==undefined)
+                 $http.get('data/enfermeros.json').then(function (response){
+                    $scope.currentEnfermero = response.data[$state.params.enfermeroId];
+                     $scope.estesi = $state.params.enfermeroId;
+                     
+                   
+                   var modResult = $state.params.franjaId % 10;
+                   
+                   var  id1= ($state.params.franjaId-modResult)/10;
+                   $scope.currentFranja=$scope.currentEnfermero.calendario[id1].grupito[modResult];
+                 $scope.currentClient=$scope.currentEnfermero.clientes[$state.params.franjaId];
+                 $scope.currentServicio=$scope.currentClient.servicios[0];
+                 });
+    }]);
+})(window.angular);
 
 
