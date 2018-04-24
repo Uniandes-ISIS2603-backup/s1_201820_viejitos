149251e2f5a883348eccc01093d7b511e49195b6
Laura Pardo <l.pardo@uniandes.edu.co>
2018-04-24 09:20:53 -0500

    la branch mas daniada

---
+++ viejitos-web/src/main/webapp/src/modules/medicos/medicos.detail.ctrl.js
@@ -0,0 +1,15 @@
+(function (ng) {
+    var mod = ng.module("medicosModule");
+    mod.constant("medicosContext", "api/medicos");
+    mod.controller('medicosDetailCtrl', ['$scope', '$http', 'medicosContext', '$state',
+        function ($scope, $http, medicosContext, $state) {
+            if (($state.params.medicoId !== undefined) && ($state.params.medicoId !== null)) {
+
+                $http.get($state.params.medicoId).then(function (response) {
+                    $scope.currentMedico = response.data;
+                });
+            }
+        }
+    ]);
+});
+(window.angular);
--- viejitos-web/src/main/webapp/src/modules/medicos/medicos.mod.js
+++ viejitos-web/src/main/webapp/src/modules/medicos/medicos.mod.js
@@ -1,21 +1,61 @@
 
 (function (ng)
 {
-    var mod= ng.module("medicosModule", ['ui.router']);
+    var mod = ng.module("medicosModule", ['ui.router']);
     mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
             var basePath = 'src/modules/medicos/';
-            $urlRouterProvider.otherwise("/medicosList");
-            $stateProvider.state('medicosList', {
-                url: '/medicos/list',
+            $urlRouterProvider.otherwise("/list");
+            $stateProvider
+                    .state('medicos', {
+                url: '/medicos',
+                abstract: true,
                 views: {
                     'mainView': {
-                        templateUrl: basePath + 'medicos.list.html',
+                        templateUrl: basePath + 'medicos.html',
                         controller: 'medicoCtrl',
                         controllerAs: 'ctrl'
                     }
                 }
-            });
-        }
-    ]);
-})(window.angular);
+            })
+                    .state('medicosList', {
+                url: '/list',
+                parent: 'medicos',
+                views: {
+                    'listView': {
+                        templateUrl: basePath + 'medicos.list.html'
+                    }
+                }
+            })
+                    .state('medicosDetail', {
+                        url: '/{medicoId:int}/detail',
+                        parent: 'medicos',
+                        param: {
+                            medicoId: null
+                        },
+                        views: {
+                            'listView': {
+                                templateUrl: basePath + 'medicos.list.html',
+                                controller: 'medicoCtrl',
+                                controllerAs: 'ctrl'
+                            },
+                            'detailView': {
+                                templateUrl: basePath + 'medicos.detail.html',
+                                controller: 'medicosDetailCtrl',
+                                controllerAs: 'ctrl2'
+                            }
+                        }
+            })
+                    .state('medicosCreate', {
+                        url: '/create',
+                        parent: 'medicos',
+                        views: {
+                            'detailView': {
+                                templateUrl: basePath + 'medicos.new.html',
+                                controller: 'medicoNewCtrl'
+                            }
+                        }})
+                    ;
+            }]);
+        });
+(window.angular);
 
