c582bfd035a28345728784ff96bbdd3a66021ca8
Laura Pardo <l.pardo@uniandes.edu.co>
2018-04-28 11:52:47 -0500

    sqls para poblar cambios a hmtl, dtos, entities

--- viejitos-web/src/main/webapp/src/modules/citas/citas.detail.ctrl.js
+++ viejitos-web/src/main/webapp/src/modules/citas/citas.detail.ctrl.js
@@ -1,7 +1,7 @@
 (function (ng) {
     var mod = ng.module("citasModule");
     mod.constant("citasContext", "api/citas");
-    mod.controller('citaDetailCtrl', ['$scope', '$http', 'citasContext', '$state',
+    mod.controller('citasDetailCtrl', ['$scope', '$http', 'citasContext', '$state',
         function ($scope, $http, citasContext, $state) {
             if (($state.params.citaId !== undefined) && ($state.params.citaId !== null)) {
 
