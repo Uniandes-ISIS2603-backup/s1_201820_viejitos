354e2f013bea19e980e2887ee5d19fef4daca09e
lf.naranjo11 <lf.naranjo11@ISIS2603S1-0081.sis.virtual.uniandes.edu.co>
2018-04-28 00:12:58 -0500

    nada :(

--- viejitos-web/src/main/webapp/src/modules/calendarios/calendarios.mod.js
+++ viejitos-web/src/main/webapp/src/modules/calendarios/calendarios.mod.js
@@ -1,35 +1,76 @@
-(function (ng) {
-    
-    
-    //crea en mod un modulo
-    var mod = ng.module("calendarioModule", ['ui.router']);
-    
-    mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
-            
-            var basePath = 'src/modules/calendarios/';
-            
-            
-            //estado por defecto
-            $urlRouterProvider.otherwise("/calendarioList");
-            
-            
-            //este es el nombre del estado a mostrar
-            $stateProvider.state('calendariosList', {
-                
-                
-                //url donde esta el estado calendariosList
-                url: '/calendarios/list',
-                 views: {
+(function(ng){
+    var mod = ng.module("calendariosModule", ['ui.router']);
+    mod.config(['$stateProvider','$urlRouterProvider', function($stateProvider, $urlRouterProvider){
+        var basePath = "src/modules/calendarios/";
+        
+        $urlRouterProvider.otherwise("/calendariosList");
+        
+        $stateProvider.state("calendarios",{
+                url: '/calendarios',
+                abstract: true,
+                views:{
                     'mainView': {
-                        //ruta del archivo del estado calendarioList en la carpeta
-                        templateUrl: basePath + 'calendarios.list.html',
-                        //nombre del controlador
-                        controller: 'calendarioCtrl',
-                        //tipo de controlador
+                            templateUrl: basePath + 'calendarios.html',
+                            controller: 'calendariosCtrl',
+                            controllerAs: 'ctrl'
+                        }
+                }
+        }).state("calendariosList",{
+                url:'/list',
+                parent:'calendarios',
+                views:{
+                    'listView':{
+                        templateUrl: basePath + 'calendarios.list.html'
+                    }
+                }
+        }).state("calendariosDetail",{
+                url:'/{calendarioId:int}/detail',
+                parent:'calendarios',
+                param:{
+                    enfermeroId:null
+                },
+                views:{
+                    'listView':{
+                        templateUrl: basePath + 'enfermeros.list.html',
+                        controller: 'enfermerosDetailCtrl',
+                        controllerAs: 'ctrl'
+                    },
+                    detailView:{
+                        templateUrl: basePath + 'enfermero.detail.html',
+                        controller: 'enfermerosDetailCtrl',
                         controllerAs: 'ctrl'
                     }
                 }
-            });
+        }).state("enfermeroFranja",{
+            url:'/{enfermeroId:int}/{franjaId:int}/detailer',
+            parent:'enfermeros',
+            param:{
+                franjaId:null,
+                enfermeroId:null
+                
+                },
+            views:{
+                'listView':{
+                    templateUrl:basePath+'enfermeros.list.html',
+                    controller:'calendariosDetailCtrl',
+                    controllerAs:'ctrl'
+                },
+                detailView:{
+                    templateUrl:basePath+'enfermero.detail.html',
+                     controller:'calendariosDetailCtrl',
+                    controllerAs:'ctrl'
+                },
+                franjaDetailView:{
+                      templateUrl:basePath+'calendario.detail.html',
+                     controller:'calendariosDetailCtrl',
+                    controllerAs:'ctrl'
+                    
+                }
+            }
+            
+            
         }
-    ]);
+        
+            );
+    }]);
 })(window.angular);
