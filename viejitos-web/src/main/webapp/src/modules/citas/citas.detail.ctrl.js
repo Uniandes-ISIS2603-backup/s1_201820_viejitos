57ef6badaeb596e95cd95dd519a54300436757cc
Laura Pardo <l.pardo@uniandes.edu.co>
2018-04-28 05:31:48 -0500

    cambios

--- viejitos-web/src/main/webapp/src/modules/citas/citas.detail.ctrl.js
+++ viejitos-web/src/main/webapp/src/modules/citas/citas.detail.ctrl.js
@@ -3,7 +3,7 @@
     mod.constant("citasContext", "api/citas");
     mod.controller('citaDetailCtrl', ['$scope', '$http', 'citasContext', '$state',
         function ($scope, $http, citasContext, $state) {
-            if (($state.params.medicoId !== undefined) && ($state.params.medicoId !== null)) {
+            if (($state.params.citaId !== undefined) && ($state.params.citaId !== null)) {
 
                 $http.get(citasContext + '/' +$state.params.citaId).then(function (response) {
                     $scope.currentCita = response.data;
