e14a0f9335b2edb00dc56c1568aaa3b0600129a4
lf.naranjo11 <lf.naranjo11@ISIS2603S1-0081.sis.virtual.uniandes.edu.co>
2018-04-28 00:12:50 -0500

    nada :(

--- viejitos-web/src/main/webapp/src/modules/enfermeros/calendario.detail.ctrl.js
+++ viejitos-web/src/main/webapp/src/modules/enfermeros/calendario.detail.ctrl.js
@@ -4,17 +4,22 @@
     mod.controller('calendariosDetailCtrl', ['$scope', '$http', 'enfermeroContext','$state',
         function($scope, $http, enfermeroContext, $state){
              if($state.params.enfermeroId!==null &&$state.params.enfermeroId!==undefined &&$state.params.franjaId!==null &&$state.params.franjaId!==undefined)
-                 $http.get('data/enfermeros.json').then(function (response){
-                    $scope.currentEnfermero = response.data[$state.params.enfermeroId];
-                     $scope.estesi = $state.params.enfermeroId;
-                     
+                 $http.get('api/enfermeros/1/calendariossemanales/').then(function (response){
+                    $scope.currentCalendario = response.data;
+                    $scope.franjasHorarias=$scope.currentCalendario.franjasHorarias;
+
+                    
                    
                    var modResult = $state.params.franjaId % 10;
                    
                    var  id1= ($state.params.franjaId-modResult)/10;
                    $scope.currentFranja=$scope.currentEnfermero.calendario[id1].grupito[modResult];
-                 $scope.currentClient=$scope.currentEnfermero.clientes[$state.params.franjaId];
-                 $scope.currentServicio=$scope.currentClient.servicios[0];
+                // $scope.currentClient=$scope.currentEnfermero.clientes[$state.params.franjaId];
+                  $scope.currentServicio=$scope.currentEnfermero.servicios[$state.params.franjaId];
+                  $scope.currentClient= $scope.currentServicio.cliente;
+                  
+                  
+            // $scope.currentServicio=$scope.currentClient.servicios[0];
                  });
     }]);
 })(window.angular);
