648024bd987b4ec1fbbbd47ddd187473af5581f3
lf.naranjo11 <lf.naranjo11@ISIS2603S1-0081.sis.virtual.uniandes.edu.co>
2018-04-28 08:54:17 -0500

    funciona latabla

--- viejitos-web/src/main/webapp/src/modules/calendarios/calendarios.ctrl.js
+++ viejitos-web/src/main/webapp/src/modules/calendarios/calendarios.ctrl.js
@@ -3,8 +3,31 @@
     mod.constant('calendarioContext','api/calendariossemanales');
     mod.controller('calendariosCtrl', ['$scope', '$http', 'calendarioContext',
         function($scope, $http, calendarioContext){
-            $http.get(calendarioContext).then(function (response){
+            $http.get('api/calendariossemanales').then(function (response){
                 $scope.calendariosRecords = response.data;
+                
+                $scope.currentCalendario=[];
+                        for(var j in $scope.calendariosRecords )
+                {
+                $scope.currentCalendario.push($scope.calendariosRecords[j]);
+
+                   // $scope.currentCalendario.franjasHorarias= $scope.calendariosRecords[j];
+                    
+                   $scope.franjasR = $scope.currentCalendario[j].franjasHorarias;
+              
+
+                    $scope.franjas = [];
+                    //grupo es lo mas grande
+
+                      for (var i = 0; i < $scope.franjasR.length; i += 5)
+                       {
+                        $scope.franjas.push($scope.franjasR.slice(i, i + 5));
+                        }
+  
+        $scope.currentCalendario[j].franjasGanadoras=$scope.franjas;     
+         $scope.currentCalendario[j].id=Number(j)+70;       
+                    }
+                
             });
     }]);
 })(window.angular);
