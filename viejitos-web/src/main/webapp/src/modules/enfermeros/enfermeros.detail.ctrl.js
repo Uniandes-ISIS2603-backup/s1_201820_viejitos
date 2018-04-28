32a11c3ed917fd7fa5d0a3df70f642ea504b9862
lf.naranjo11 <lf.naranjo11@ISIS2603S1-0081.sis.virtual.uniandes.edu.co>
2018-04-28 00:12:31 -0500

    nada :(

--- viejitos-web/src/main/webapp/src/modules/enfermeros/enfermeros.detail.ctrl.js
+++ viejitos-web/src/main/webapp/src/modules/enfermeros/enfermeros.detail.ctrl.js
@@ -1,13 +1,29 @@
-(function(ng){
+(function (ng) {
     var mod = ng.module('enfermerosModule');
-    mod.constant('enfermeroContext','api/enfermeros');
-    mod.controller('enfermerosDetailCtrl', ['$scope', '$http', 'enfermeroContext','$state',
-        function($scope, $http, enfermeroContext, $state){
-             if($state.params.enfermeroId!==null && $state.params.enfermeroId!==undefined)
-                 $http.get(enfermeroContext + '/' + $state.params.enfermeroId).then(function (response){
-                    $scope.currentEnfermero = response.data[0];
-            });
-    }]);
+    mod.constant('enfermeroContext', 'api/enfermeros/1/calendariossemanales/');
+    mod.controller('enfermerosDetailCtrl', ['$scope', '$http', 'enfermeroContext', '$state',
+        function ($scope, $http, enfermeroContext, $state) {
+            if ($state.params.enfermeroId !== null && $state.params.enfermeroId !== undefined)
+                $http.get('api/enfermeros/1/calendariossemanales/').then(function (response) {
+                    console.log($scope);
+
+                    $scope.currentCalendario = response.data;
+
+                    $scope.franjasR = $scope.currentCalendario.franjasHorarias;
+                    console.log($scope);
+
+                    $scope.franjas = [];
+                    //grupo es lo mas grande
+
+                    for (var i = 0; i < $scope.franjasR.length; i += 5)
+                    {
+                        $scope.franjas.push($scope.franjasR.slice(i, i + 5));
+
+                    }
+
+
+                });
+        }]);
 })(window.angular);
 
 
