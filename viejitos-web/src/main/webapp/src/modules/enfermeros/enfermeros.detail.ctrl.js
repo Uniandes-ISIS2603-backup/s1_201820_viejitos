0a3676c2a8137b3ed38fccc61c16dcc560d12522
js.espitia <js.espitia@ISIS2603S1-0090.sis.virtual.uniandes.edu.co>
2018-04-27 19:02:15 -0500

    calificacion con detailDTO

--- viejitos-web/src/main/webapp/src/modules/enfermeros/enfermeros.detail.ctrl.js
+++ viejitos-web/src/main/webapp/src/modules/enfermeros/enfermeros.detail.ctrl.js
@@ -3,8 +3,8 @@
     mod.constant('enfermeroContext','api/enfermeros');
     mod.controller('enfermerosDetailCtrl', ['$scope', '$http', 'enfermeroContext','$state',
         function($scope, $http, enfermeroContext, $state){
-             if($state.params.enfermeroId!==null &&$state.params.enfermeroId!==undefined)
-                 $http.get('data/enfermeros.json').then(function (response){
+             if($state.params.enfermeroId!==null && $state.params.enfermeroId!==undefined)
+                 $http.get(enfermeroContext + '/' + $state.params.enfermeroId).then(function (response){
                     $scope.currentEnfermero = response.data[0];
             });
     }]);
