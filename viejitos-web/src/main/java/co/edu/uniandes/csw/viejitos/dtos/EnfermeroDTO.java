200f1f54deac8e68bb90833fb39e2af09a7bb665
lf.naranjo11 <lf.naranjo11@ISIS2603S1-0081.sis.virtual.uniandes.edu.co>
2018-05-03 11:35:06 -0500

    CORRECIONES ID ENFERMERO APARECE

--- viejitos-web/src/main/java/co/edu/uniandes/csw/viejitos/dtos/EnfermeroDTO.java
+++ viejitos-web/src/main/java/co/edu/uniandes/csw/viejitos/dtos/EnfermeroDTO.java
@@ -72,9 +72,17 @@ public class EnfermeroDTO {
         this.login = login;
     }
 
+     public Long getId() {
+        return id;
+    }
+    
+    
     public void setId(Long id) {
         this.id = id;
     }
+    
+   
+    
 
     public String getNombre() {
         return nombre;
