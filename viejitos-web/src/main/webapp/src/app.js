1401ce2f500c985a75b77bc87b57a3f4570eea22
lf.naranjo11 <lf.naranjo11@ISIS2603S1-0081.sis.virtual.uniandes.edu.co>
2018-05-15 17:35:06 -0500

    ya esta todo el login.solo falta poner a los estados que se quedan un data que tenga roles y requirelogin

--- viejitos-web/src/main/webapp/src/app.js
+++ viejitos-web/src/main/webapp/src/app.js
@@ -21,10 +21,55 @@
             $qProvider.errorOnUnhandledRejections(false);
         }]);
    
-    
-    
-    
-    
+     app.run(['$rootScope',"$state", function ($rootScope,$state) {
+             
+             
+             console.log($state.current.data);
+             
+           
+                 
+               
+                var requireLogin = false
+                     //   $state.current.data.requireLogin
+                var roles = []
+                //$state.current.data.roles
+ 
+                /**
+                 * @ngdoc function
+                 * @name isAuthenticated
+                 * @methodOf mainApp.module:mainApp
+                 * @description Esta función define si el usuario se encuentra
+                 * dentro de su cuenta.
+                 * @returns {Boolean} Verdadero si está dentro de su cuenta.
+                 */
+                $rootScope.isAuthenticated = function () {
+
+                    if (sessionStorage.getItem("username") != null) {
+                        $rootScope.currentUser = sessionStorage.getItem("name");
+                        return true;
+                    } else {
+                        return false;
+                    }
+                };
+              
+                $rootScope.hasPermissions = function () {
+                    if (($rootScope.isAuthenticated) && (roles.indexOf(sessionStorage.getItem("rol")) > -1)) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                };
+
+
+                if (requireLogin && (sessionStorage.getItem("username") === null)) {
+                    event.preventDefault();
+                    $state.go('login', $state.params);
+                }
+
+            
+
+        }]);   
+ 
     
 })(window.angular);
 
