c84fdebaa9a36c0a9db67e660c2644c52f3a9341
js.espitia <js.espitia@ISIS2603S1-0090.sis.virtual.uniandes.edu.co>
2018-05-15 21:22:22 -0500

    Cambio masivo, imagenes en carpeta local, css en carpeta local, página de inicio creada pero sin transición de estados para volver y sin despliegue de informacion de integrantes, modulo de solicitud de servicio creado pero no conectado,

--- viejitos-web/src/main/webapp/src/app.js
+++ viejitos-web/src/main/webapp/src/app.js
@@ -14,7 +14,7 @@
         'medicosModule',
         'calendariosModule',
         'historialServiciosModule',
-                'loginModule'
+        'loginModule'
     ]);
     // Resuelve problemas de las promesas
     app.config(['$qProvider', function ($qProvider) {
