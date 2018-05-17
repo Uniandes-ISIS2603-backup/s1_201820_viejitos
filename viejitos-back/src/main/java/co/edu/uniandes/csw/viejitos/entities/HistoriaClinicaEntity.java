bca5840d270bc35610752cc34f1ebdcbd29699ef
Laura Pardo <l.pardo@uniandes.edu.co>
2018-05-17 07:17:39 -0500

    Unused imports 4.0

--- viejitos-back/src/main/java/co/edu/uniandes/csw/viejitos/entities/HistoriaClinicaEntity.java
+++ viejitos-back/src/main/java/co/edu/uniandes/csw/viejitos/entities/HistoriaClinicaEntity.java
@@ -7,9 +7,7 @@ package co.edu.uniandes.csw.viejitos.entities;
 
 import java.io.Serializable;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
 import uk.co.jemos.podam.common.PodamExclude;
 /**
  *
